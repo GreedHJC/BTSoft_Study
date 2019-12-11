da746529ab89bfa522ecba20e3f314ef154ff0fd
Qweqw <qweqwe472@naver.com>
2019-12-11 10:13:58 +0900

    191211_106P_Mybatis와 스프링 연동 및 Log4j 설정

---
+++ ex00/src/test/java/kr/btsoft/persistence/JDBCTests.java
@@ -0,0 +1,36 @@
+package kr.btsoft.persistence;
+
+import lombok.extern.log4j.Log4j;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import static org.junit.Assert.fail;
+
+@Log4j
+public class JDBCTests {
+    static {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testConnection() {
+
+        try (Connection con =
+                DriverManager.getConnection(
+                        "jdbc:oracle:thin:@localhost:1521:orcl",
+                        "book_ex",
+                        "bts5735")){
+
+            log.info(con);
+        } catch (Exception e){
+            fail(e.getMessage());
+        }
+
+    }
+}
---
+++ ex00/src/test/java/kr/btsoft/persistence/TimeMapperTests.java
@@ -0,0 +1,34 @@
+package kr.btsoft.persistence;
+
+import kr.btsoft.mapper.TimeMapper;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+// java 설정의 경우
+// @ContextConfiguration(classes = { kr.btsoft.config.RootConfig.class })
+@Log4j
+public class TimeMapperTests {
+    @Setter(onMethod_ = @Autowired)
+    private TimeMapper timeMapper;
+
+    @Test
+    public void testGetTime() {
+        log.info(timeMapper.getClass().getName());
+        log.info(timeMapper.getTime());
+    }
+
+    @Test
+    public void tetGetTime2() {
+        log.info("getTime2");
+//        log.info(timeMapper.getTime2());
+
+    }
+
+}
