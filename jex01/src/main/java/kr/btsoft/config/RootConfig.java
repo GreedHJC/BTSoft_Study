dc37729968b12315387374387b2f926e95720268
Qweqw <qweqwe472@naver.com>
2019-12-16 19:25:42 +0900

    test- Part3 기본적입 웹 게시물 관리 ~ Chapter09 비즈니스계층

--- jex01/src/main/java/kr/btsoft/config/RootConfig.java
+++ jex01/src/main/java/kr/btsoft/config/RootConfig.java
@@ -18,21 +18,24 @@ import javax.sql.DataSource;
 public class RootConfig {
 
     @Bean
-    public SqlSessionFactory sqlSessionFactory() throws  Exception{
-        SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
-        sqlSessionFactory.setDataSource((dataSource()));
-        return (SqlSessionFactory) sqlSessionFactory.getObject();
-    }
-    @Bean
     public DataSource dataSource() {
         HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
-        hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:orcl");
+        hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+        hikariConfig.setJdbcUrl("jdbc:log4jdbc:oracle:thin:@localhost:1521:orcl");
+//        hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+//        hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:orcl");
         hikariConfig.setUsername("book_ex");
         hikariConfig.setPassword("bts5735");
 
         HikariDataSource dataSource = new HikariDataSource(hikariConfig);
 
         return dataSource;
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws  Exception{
+        SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+        sqlSessionFactory.setDataSource((dataSource()));
+        return (SqlSessionFactory) sqlSessionFactory.getObject();
     }
 }
---
+++ jex01/src/main/java/kr/btsoft/domain/BoardVO.java
@@ -0,0 +1,16 @@
+package kr.btsoft.domain;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class BoardVO {
+    private Long bno;
+    private String title;
+    private String content;
+    private String writer;
+    private Date regdate;
+    private Date updateDate;
+
+}
